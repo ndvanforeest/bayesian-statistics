43d31eaeaafbbb450759d0f9fe3d94424a84dae1
Nicky van Foreest <vanforeest@gmail.com>
2021-05-26 13:53:57 +0200

    code 5

--- code/bag.py
+++ code/bag.py
@@ -1,18 +1,16 @@
 import numpy as np
-from scipy.stats import bernoulli
 
 from tree_simple import Tree
-from bag import Bag
 
 rng = np.random.default_rng(3)
 
-class RandomForest(Bag):
-    min_size = 1
-    max_depth = 0
-
-    def __init__(self, min_size, max_depth, n_trees, n_features):
-        super().__init__(min_size, max_depth, n_trees)
-        self.n_features = n_features
+class Bag:
+    def __init__(self, min_size, max_depth, n_trees):
+        self.n_trees = n_trees
+        self.min_size = min_size
+        self.max_depth = max_depth
+        self.trees = []
+        self.trees = []
 
     def fit(self, X, Y):
         self.X, self.Y = X, Y
@@ -20,10 +18,19 @@ class RandomForest(Bag):
         for _ in range(self.n_trees):
             bootstrap = rng.integers(low=0, high=n, size=n)
             X, Y = self.X[bootstrap], self.Y[bootstrap]
-            features = rng.choice(range(p), size=self.n_features, replace=False)
-            tree = Tree(min_size = self.min_size, max_depth = self.max_depth)
-            tree.fit(X[:, features], Y)
+            tree = Tree(min_size=self.min_size, max_depth=self.max_depth)
+            tree.fit(X, Y)
             self.trees.append(tree)
+
+    def majority_vote(self, X):
+        predictions = np.array([t.predict(X) for t in self.trees])
+        return np.sign(predictions.sum(axis=0))
+    
+    
+    def predict(self, X):
+        return self.majority_vote(X)
+
+    
 
 def test():
     X = np.array(
@@ -42,15 +49,14 @@ def test():
     )
     Y = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
 
-    RandomForest.max_depth = 2
-    RandomForest.min_size = 1
-    rf = RandomForest(max_depth=2, min_size=1, n_trees=51, n_features=1)
-    rf.fit(X, Y)
+    bag = Bag(min_size=1, max_depth=2, n_trees=5)
+    bag.fit(X, Y)
+    print(bag.predict(X))
 
-    tests = [[1, 10], [4, -5], [6, 1], [7, 2], [8, 5]]
+    tests = [[3, 10], [4, -5], [6, 1], [7, 2], [8, 5]]
     for x in tests:
-        x = np.array(x).reshape(1,2)
-        print(f"Predicted class of {x}: {rf.predict(x)}")
+        x = np.array(x).reshape(1, 2)
+        print(f"Predicted class of {x}: {bag.predict(x)}")
 
 if __name__ == "__main__":
     test()
